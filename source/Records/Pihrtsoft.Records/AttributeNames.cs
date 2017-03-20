8a57bf7f0b818f26e05e50f3320bca1699e902f6
Josef Pihrt <josef.pihrt@outlook.com>
2017-03-20 10:34:21 +0100

    Remove unused constants.

--- source/Records/Pihrtsoft.Records/AttributeNames.cs
+++ source/Records/Pihrtsoft.Records/AttributeNames.cs
@@ -7,8 +7,6 @@ namespace Pihrtsoft.Records
         public const string Name = nameof(Name);
         public const string Value = nameof(Value);
         public const string Id = nameof(Id);
-        public const string Property = nameof(Property);
-        public const string Type = nameof(Type);
         public const string Tag = nameof(Tag);
         public const string DefaultValue = nameof(DefaultValue);
         public const string IsCollection = nameof(IsCollection);
