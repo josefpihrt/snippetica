8e8d3432c0d61b3d709c9f00a4e894019c54f34e
Josef Pihrt <josef.pihrt@outlook.com>
2017-12-12 04:20:34 +0100

    Add GlobalSuppressions.

---
+++ source/Records/Pihrtsoft.Records/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "<Pending>", Scope = "member", Target = "~P:Pihrtsoft.Records.EntityDefinition.PropertiesText")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "<Pending>", Scope = "member", Target = "~P:Pihrtsoft.Records.Record.DebuggerDisplay")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "<Pending>", Scope = "member", Target = "~P:Pihrtsoft.Records.TagCollection.TagsText")]
+
