08049e95fe687008adb3f2a01b4a7aebe077cee9
Josef Pihrt <josef@pihrt.net>
2023-08-10 10:55:27 +0200

    update

--- src/Snippetica.VisualStudio/Comparers/NamespaceComparer.cs
+++ src/Snippetica.VisualStudio/Comparers/NamespaceComparer.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System;
 
 namespace Snippetica.VisualStudio;
 
