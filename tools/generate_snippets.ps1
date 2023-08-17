0667dbe2327003e90f7c74bb3c714326bb893117
Josef Pihrt <josef@pihrt.net>
2023-08-17 18:04:11 +0200

    update

--- tools/generate_snippets.ps1
+++ tools/generate_snippets.ps1
@@ -3,4 +3,4 @@ dotnet build "../src/Snippetica.CodeGeneration.SnippetGenerator/SnippetGenerator
 
 if(!$?) { Read-Host; Exit }
 
-& "../src/Snippetica.CodeGeneration.SnippetGenerator/bin/Release/net6.0/Snippetica.CodeGeneration.SnippetGenerator" "../src" "../src/Snippetica.CodeGeneration.Metadata/data"
+& "../src/Snippetica.CodeGeneration.SnippetGenerator/bin/Release/net6.0/Snippetica.CodeGeneration.SnippetGenerator" "../src" "../src/Snippetica.CodeGeneration.Metadata/Data"
